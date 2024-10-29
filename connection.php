f759005f29c2a051c7f601516401bf1d9a5048d7
roroRathod <136560629+roroRathod@users.noreply.github.com>
2024-10-29 21:56:50 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,3 +1,16 @@
 <?php 
-$con = mysqli_connect('localhost:3308', 'root', '', 'SoulMate');
+$servername = "localhost";
+$username = "root"; // Default XAMPP MySQL username
+$password = ""; // Leave blank unless you've set a password for MySQL
+$dbname = "soulmate";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} else {
+    echo "Connected successfully";
+}
 ?>
